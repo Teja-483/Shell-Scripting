29ea2f859810f21c4b56940fc8f133723a5da7f1
Ubuntu <ubuntu@ip-172-31-12-200.ap-southeast-2.compute.internal>
2025-03-21 11:05:24 +0000

    After merging the both base_branch & cherrypickbrnach we face merge conflict, opened the file and added modified the head line manually and saved the file,git add,git commit given

--- firstfile.sh
+++ firstfile.sh
@@ -1,2 +1,3 @@
 FIrst line so consider its main repo
 now Adding second line in the base_branch of the firstfile after the cherrypick commit is completed
+Adding changes related to the cherrypick test.
